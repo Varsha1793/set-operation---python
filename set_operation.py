c01de858bcecd72e716e54a9c14acdc322ff49ac
Varsha.P <100304556+Varsha1793@users.noreply.github.com>
2022-07-11 20:40:04 +0530

    Add files via upload

---
+++ set_operation.py
@@ -0,0 +1,7 @@
+set1={1,2,3,4,5}
+set2={2,4,6,8,10}
+print("Union of set1 and set2:",set1.union(set2))
+print("Intersection between set1 and set2:",set1.intersection(set2))
+print("Difference between set1 and set2:",set1.difference(set2))
+print("Symmetric difference between set1 to set2:",set1.symmetric_difference(set2))
+print("Symmetric difference between set2 to set1:",set2.symmetric_difference(set1))
